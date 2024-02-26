05689537dc6a28bc8d24553a5a652dd93933d0b8
zurnadurumm <kaanc645@gmail.com>
2024-02-27 00:34:06 +0400

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header>
+        <div className='flex items-center justify-between px-9 py-4.5'>
+            <div>
+                <Link href={'/'}>
+                    <Image 
+                        src={'/logo.svg'}
+                        alt='Logo'
+                        width={100}
+                        height={100}
+                        priority
+                    />
+                </Link>
+  
+            </div>
+            <div className='flex gap-12 font-semibold'>
+                <Link href={'/'} className='transition-all text-Text hover:text-transparent hover:bg-gradient-to-r hover:from-Primary hover:to-Accent bg-Text bg-clip-text'>Home</Link>
+                <Link className='transition-all text-Text hover:text-transparent hover:bg-gradient-to-r hover:from-Primary hover:to-Accent bg-Text bg-clip-text' href={'/contact'}>Contact</Link>
+                <Link className='transition-all text-Text hover:text-transparent hover:bg-gradient-to-r hover:from-Primary hover:to-Accent bg-Text bg-clip-text' href={'/blog'}>Blog</Link>
+                
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      "colors": {
+        "Text": "#e7e8ee",
+        "Primary": "#9ca0cd",
+        "Background": "#09090e",
+        "Secondary": "#333a7a",
+        "Accent": "#5b65c7"
+       },
+       "borderRadius": {
+        "none": "0",
+        "xs": "1rem"
+       },
+       padding:{
+        '4.5': '1.125rem'
+       }
     },
   },
   plugins: [],
